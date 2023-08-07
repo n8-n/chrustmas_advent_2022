6709e9d76ba9807de4efeb035da10698624fd981
Nathan Flynn <4656318+n8-n@users.noreply.github.com>
2023-08-07 17:41:16 +0100

    Add main functionality to choose exercise

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![allow(dead_code)]
+
+use std::io;
+
 fn main() {
-    println!("Hello, Advent of Code!");
+    println!("Which exercise answer would you like? [1-25]");
+
+    let mut choice = String::new();
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Could not read line");
+
+    let choice: u32 = choice.trim().parse().expect("Not a number!");
+
+    if choice < 1 || choice > 25 {
+        println!("Not a valid exercise number. Should be 1-25.");
+    } else {
+        println!("{:?}", choice);
+    }
 }
